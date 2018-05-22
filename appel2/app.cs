b8561b6f40c66cf8abc50f9566ef39a6009bfcea
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-05-22 20:12:22 +0700

    a

--- appel2/app.cs
+++ appel2/app.cs
@@ -21,45 +21,127 @@ namespace appel
     PermissionSet(SecurityAction.InheritanceDemand, Name = "FullTrust")]
     public class app
     {
+        #region [ VARIABLE ]
+
         public const int m_item_width = 120;
         public const int m_item_height = 90;
 
         public const int m_box_width = 320;
-        public const int m_box_height = 180; 
+        public const int m_box_height = 180;
 
         public const int m_app_width = m_box_width * 2 + 29;
         public const int m_app_height = 590;
 
         public const int m_player_width = 640;
         public const int m_player_height = 360;
-         
-        
+
         static fMain main;
+
+        #endregion
+
+        #region [ API ]
 
         static ConcurrentDictionary<string, IthreadMsg> dicService = null;
         static ConcurrentDictionary<string, msg> dicResponses = null;
 
+        static ConcurrentQueue<msg> api_msg_queue = null;
+        static System.Threading.Timer api_msg_timer = null;
+
         public static void postToAPI(msg m)
         {
-            if (dicService.ContainsKey(m.API))
-            {
-                IthreadMsg sv;
-                if (dicService.TryGetValue(m.API, out sv))
-                {
-                    new Thread(new ParameterizedThreadStart((object _sv) =>
-                    {
-                        IthreadMsg so = (IthreadMsg)_sv;
-                        so.Execute(m);
-                    })).Start(sv);
-                }
-
-            }
+            api_msg_queue.Enqueue(m);
         }
 
         public static void postToAPI(string api, string key, object input)
         {
             postToAPI(new msg() { API = api, KEY = key, Input = input });
         }
+
+        private static void f_api_init() {
+            api_msg_queue = new ConcurrentQueue<msg>();
+            if (api_msg_timer == null) 
+                api_msg_timer = new System.Threading.Timer(new System.Threading.TimerCallback((obj) =>
+                {
+                    if (api_msg_queue.Count > 0)
+                    {
+                        msg m = null;
+                        if (api_msg_queue.TryDequeue(out m) && m != null)
+                        {
+                            if (!string.IsNullOrEmpty(m.API) && dicService.ContainsKey(m.API))
+                            {
+                                IthreadMsg sv;
+                                if (dicService.TryGetValue(m.API, out sv) && sv != null)
+                                {
+                                    ////new Thread(new ParameterizedThreadStart((object _sv) =>
+                                    ////{
+                                    ////    IthreadMsg so = (IthreadMsg)_sv;
+                                    ////    so.Execute(m);
+                                    ////})).Start(sv);
+                                    sv.Execute(m);
+                                }
+                            }
+                        }
+                    }
+                }), null, 100, 100);
+
+            dicResponses = new ConcurrentDictionary<string, msg>();
+            dicService = new ConcurrentDictionary<string, IthreadMsg>();
+            
+            //dicService.TryAdd(_API.WORD, new threadMsg(new api_word()));
+            //dicService.TryAdd(_API.MEDIA, new threadMsg(new api_media()));
+        }
+
+        #endregion
+
+        #region [ MAIN ]
+
+        private static void f_main_init() {
+            main = new fMain();
+            main.Shown += main_Shown;
+            main.FormClosing += main_Closing;
+            Application.EnableVisualStyles();
+            Application.Run(main);
+        }
+
+        private static void main_Closing(object sender, FormClosingEventArgs e)
+        {
+            if (main == null) return;
+
+            var confirmResult = MessageBox.Show("Are you sure to exit this application ?", "Confirm Exit!", MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes)
+            {
+                foreach (var kv in dicService)
+                    if (kv.Value != null)
+                        kv.Value.Stop();
+
+                main.f_form_freeResource();
+
+                // wait for complete threads, free resource
+                Thread.Sleep(200);
+
+                //Application.ExitThread();
+                //Application.Exit();
+            }
+            else
+                e.Cancel = true;
+        }
+
+        private static void main_Shown(object sender, EventArgs e)
+        {
+            main.Width = m_app_width;
+            main.Height = m_app_height;
+            main.Top = (Screen.PrimaryScreen.WorkingArea.Height - main.Height) / 2;
+            main.Left = (Screen.PrimaryScreen.WorkingArea.Width - main.Width) / 2;
+        }
+
+        public static IFORM get_Main()
+        {
+            return main;
+        }
+
+        #endregion
+
+        #region [ APP ]
 
         static app()
         {
@@ -100,71 +182,15 @@ namespace appel
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | (SecurityProtocolType)3072 | (SecurityProtocolType)0x00000C00 | SecurityProtocolType.Tls;
             // registry models to protofuf
             RuntimeTypeModel.Default.Add(typeof(DateTimeOffset), false).SetSurrogate(typeof(DateTimeOffsetSurrogate));
-            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-            dicResponses = new ConcurrentDictionary<string, msg>();
-            dicService = new ConcurrentDictionary<string, IthreadMsg>();
-
-            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            dicService.TryAdd(_API.WORD, new threadMsg(new api_word()));
-            dicService.TryAdd(_API.MEDIA, new threadMsg(new api_media()));
-
-
-            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            main = new fMain();
-            main.Shown += main_Shown;
-            main.FormClosing += main_Closing;
-            Application.EnableVisualStyles();
-            Application.Run(main);
+            f_api_init();
+            f_main_init();
         }
 
         public static void Exit()
         {
             //main.f_free_Resource();
             Application.Exit();
-        }
-
-        #region [ MAIN ]
-
-        private static void main_Closing(object sender, FormClosingEventArgs e)
-        {
-            if (main == null) return;
-
-            var confirmResult = MessageBox.Show("Are you sure to exit this application ?", "Confirm Exit!", MessageBoxButtons.YesNo);
-            if (confirmResult == DialogResult.Yes)
-            {
-                foreach (var kv in dicService)
-                    if (kv.Value != null)
-                        kv.Value.Stop();
-
-                main.f_form_freeResource();
-
-                // wait for complete threads, free resource
-                Thread.Sleep(200);
-
-                //Application.ExitThread();
-                //Application.Exit();
-            }
-            else
-                e.Cancel = true;
-        }
-
-        private static void main_Shown(object sender, EventArgs e)
-        {
-            main.Width = m_app_width;
-            main.Height = m_app_height;
-            main.Top = (Screen.PrimaryScreen.WorkingArea.Height - main.Height) / 2;
-            main.Left = (Screen.PrimaryScreen.WorkingArea.Width - main.Width) / 2;
-        }
-
-        #endregion
-
-        #region
-
-        public static IFORM get_Main()
-        {
-            return main;
         }
         
         #endregion
